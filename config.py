cf6d3e755bbc66cfd239725f4c96714439c127d0
Andreas Pfeiffer <andreas.pfeiffer@cern.ch>
2018-11-08 15:30:57 +0100

    move to use instance-specific config file for sensitive values.

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-SECRET_KEY = 'aVerySecretAndHardToGuessKey'
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///data-test.sqlite'
-
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# Move the following two lines into the instance/config.py and adapt as needed:
+SECRET_KEY = 'secretKey'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///data-test.sqlite'
 
